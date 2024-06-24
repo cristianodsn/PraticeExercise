ed4ce4108859faf4364e563c345cbda5349dc734
Cristiano Castiel <cristianoct03@gmail.com>
2024-06-24 14:50:33 -0300

    Finished

---
+++ Rascunho/Program.cs
@@ -0,0 +1,48 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Enter the values of matrix (n, m): ");
+string[] valuesDimension = Console.ReadLine().Split(","); 
+int n = int.Parse(valuesDimension[0]);
+int m = int.Parse(valuesDimension[1]);
+int[,] mat = new int[n, m];
+
+for (int i = 0; i < n; i++)
+{
+    string [] aux = Console.ReadLine().Split(' ');
+
+    for (int j = 0; j< m; j++)
+    {
+        mat[i, j] = int.Parse(aux[j]);
+    }
+}
+
+Console.WriteLine("Enter the number:");
+int number = int.Parse(Console.ReadLine());
+
+for (int i = 0; i < n; i++)
+{
+    for (int j = 0; j < m; j++)
+    {
+        if (mat[i, j] == number)
+        {
+            Console.WriteLine("Position: " + i + ", " + j);
+            if (j > 0)
+            {
+                Console.WriteLine("Left: " + mat[i, j -1]);
+            }
+            if (j < m - 1)
+            {
+                Console.WriteLine("Right: " + mat[i, j + 1]);
+            }
+            if(i > 0)
+            {
+                Console.WriteLine("UP: " + mat[i -1, j]);
+            }
+            if(i < n - 1)
+            {
+                Console.WriteLine("Down: " + mat[i + 1, j]);
+            }
+        }
+    }
+}
+
